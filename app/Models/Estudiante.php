ac2d51663089fda94e44a9b5a7845cb9dc9fccbf
RodrigoMedina <rodrigo.medina.2905@gmail.com>
2025-10-29 14:42:13 -0500

    added_model_apoderado

--- app/Models/Estudiante.php
+++ app/Models/Estudiante.php
@@ -3,27 +3,37 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+use App\Models\Seccion;
+use App\Models\Apoderado;
 
 class Estudiante extends Model
 {
     #protected $table = 'estudiantes';
 
     protected $fillable = [
-        'tipo_documento',
+        'tipo_documento', #enum
         'nro_documento',
         'nombres',
         'apellido_paterno',
         'apellido_materno',
-        'genero',
-        'estado_civil',
+        'genero', #enum
+        'estado_civil', #enum
         'fecha_nacimiento',
         'telefono',
         'direccion',
         'email',
     ];
 
-    public function secciones()
+    public function secciones() : BelongsToMany
     {
         return $this->belongsToMany(Seccion::class, 'estudiante_seccion');
+    }
+
+    public function apoderado() : BelongsTo
+    {
+        return $this->belongsTo(Apoderado::class);
     }
 }
